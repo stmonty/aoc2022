952ca91709080458d1f90c0f7a261b6731af8d63
stmonty <stmontydev@gmail.com>
2022-11-30 23:58:32 -0600

    Finished Day1. Haskell is fun but tough.

---
+++ day1.hs
@@ -0,0 +1,19 @@
+module Main where
+import Control.Monad (join)
+import Data.List
+
+strToInt :: [String] -> [Int]
+strToInt = map read
+
+parseInput :: String -> [String]
+parseInput s = let 
+    lined = lines s
+    separated = lines $ join [if null line then "\n" else line ++ " " 
+                                    | line <- lined]
+    in separated
+main :: IO ()
+main = do
+  parsedContent <- parseInput <$> readFile "input1.txt"
+  let ans1 = map (sum . strToInt . words) parsedContent
+  let ans2 = sum $ take 3 $ reverse (sort ans1)
+  print (maximum ans1, ans2)
